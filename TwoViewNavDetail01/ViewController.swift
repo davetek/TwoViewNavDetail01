543fe18aeed8e75727c6628572bbd25f3b76e4e0
davetek <dlawrence4994@gmail.com>
2018-08-26 17:49:43 -0400

    Initial Commit

---
+++ TwoViewNavDetail01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoViewNavDetail01
+//
+//  Created by David Lawrence on 8/26/18.
+//  Copyright © 2018 clarity for action. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
